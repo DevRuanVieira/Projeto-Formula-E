849953aa228afaf645a712170b0c77e5410993f3
DevRuanX <160757151+DevRuanX@users.noreply.github.com>
2024-06-17 14:29:40 -0300

    Delete FormulaE directory

--- FormulaE/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector('#btn-menu');
-const boxMenuItens = document.querySelector('.box-menu-itens');
-
-menu.addEventListener('click', (e) =>{
-    e.preventDefault();
-    boxMenuItens.classList.toggle('open')
-    menu.classList.add('lines')
-})
